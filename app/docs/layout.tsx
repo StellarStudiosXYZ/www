fc4c5d0dda9da90690697629a0feb33388281f16
sarthakjainxyz <jsarthak2007@gmail.com>
2026-01-14 17:07:53 +0530

    Somewhat Structure for docs, and also tried to write installation guide for MC Mods

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,31 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
-  return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  );
+export default function Layout({ children }: LayoutProps<"/docs">) {
+    return (
+        <DocsLayout
+            tree={source.getPageTree()}
+            githubUrl="https://github.com/StellarStudiosXYZ"
+            sidebar={{
+                tabs: [
+                    {
+                        title: "Pterodactyl",
+                        url: "/docs/pterodactyl",
+                    },
+                    {
+                        title: "Blueprint",
+                        url: "/docs/blueprint",
+                    },
+                    {
+                        title: "Paymenter",
+                        url: "/docs/paymenter",
+                    },
+                ],
+            }}
+            {...baseOptions()}
+        >
+            {children}
+        </DocsLayout>
+    );
 }
